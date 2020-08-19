4e6e269e4f245b23df80fc6f4c567f79357dabcd
manuGil <gilbert.mg22@gmail.com>
2020-08-19 11:34:09 +0200

    remove 'edit on buttons'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,7 @@ html_static_path = ['_static']
 # Costume css. Paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    'css/core.css',
+    'css/custom.css',
 ]
 
 # Latex Costume configurations
